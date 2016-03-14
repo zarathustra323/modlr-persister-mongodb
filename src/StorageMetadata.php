462b3f3ab5dbc35f6fa6625f7474e02178fc5707
Jacob Bare <jacob.bare@cygnus.com>
2016-03-14 09:00:57 -0500

    Initial commit

---
+++ src/StorageMetadata.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace As3\Modlr\RestOdm\Persister\MongoDb;
+
+use As3\Modlr\RestOdm\Metadata\Interfaces\MergeableInterface;
+use As3\Modlr\RestOdm\Metadata\Interfaces\StorageLayerInterface;
+
+/**
+ * Defines the MongoDB storage metadata for an entity (e.g. a database object).
+ * Should be loaded using the MetadataFactory, not instantiated directly.
+ *
+ * @author Jacob Bare <jacob.bare@gmail.com>
+ */
+class StorageMetadata implements StorageLayerInterface
+{
+    /**
+     * The storage layer key.
+     *
+     * @var string
+     */
+    public $persisterKey;
+
+    /**
+     * The database name.
+     *
+     * @var string
+     */
+    public $db;
+
+    /**
+     * The collection name.
+     *
+     * @var string
+     */
+    public $collection;
+
+    /**
+     * The ID strategy to use.
+     * Currently object is the only valid choice.
+     *
+     * @todo Implement an auto-increment integer id strategy.
+     * @var string
+     */
+    public $idStrategy = 'object';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getKey()
+    {
+        return $this->persisterKey;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function merge(MergeableInterface $metadata)
+    {
+        return $this;
+    }
+}
